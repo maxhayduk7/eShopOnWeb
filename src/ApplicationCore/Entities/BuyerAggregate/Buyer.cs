284c11faa93a6e0553c4663db63b18a38689cfa1
Steve Smith <steve@kentsmiths.com>
2020-12-06 23:41:16 -0500

    Added address verification

--- src/ApplicationCore/Entities/BuyerAggregate/Buyer.cs
+++ src/ApplicationCore/Entities/BuyerAggregate/Buyer.cs
@@ -1,5 +1,6 @@
 ﻿using Ardalis.GuardClauses;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using System;
 using System.Collections.Generic;
 
 namespace Microsoft.eShopWeb.ApplicationCore.Entities.BuyerAggregate
@@ -28,15 +29,32 @@ namespace Microsoft.eShopWeb.ApplicationCore.Entities.BuyerAggregate
         public string City { get; private set; }
         public string State { get; private set; }
         public string ZipCode { get; private set; }
+        public string AddressVerified { get; private set; } = string.Empty;
 
         public void UpdateAddress(string street1, string street2, string city, string state, string zipCode)
         {
-            // TODO: Validate address
+            AddressVerified = VerifyAddress(street1, street2, city, state, zipCode);
+
             Street1 = street1;
             Street2 = street2;
             City = city;
             State = state;
             ZipCode = zipCode;
+        }
+
+        private string VerifyAddress(string street1, string street2, string city, string state, string zipCode)
+        {
+            string missing = "Missing Field: ";
+            if (string.IsNullOrEmpty(street1)) return missing + nameof(Street1);
+            if (string.IsNullOrEmpty(city)) return missing + nameof(City);
+            if (string.IsNullOrEmpty(state)) return missing + nameof(State);
+            if (string.IsNullOrEmpty(zipCode)) return missing + nameof(ZipCode);
+
+            string mismatch = "Invalid State-ZipCode combination";
+            if (state != "NY" && zipCode == "12345") return mismatch;
+            if (state != "OH" && zipCode == "44240") return mismatch;
+
+            return "Verified";
         }
     }
 }
