cc9c3dae8d377e2efce6ee0a1b0ddc5d2d18fdb1
Steve Smith <steve@kentsmiths.com>
2020-12-06 22:59:56 -0500

    Add address to buyer

--- src/ApplicationCore/Entities/BuyerAggregate/Buyer.cs
+++ src/ApplicationCore/Entities/BuyerAggregate/Buyer.cs
@@ -22,5 +22,21 @@ namespace Microsoft.eShopWeb.ApplicationCore.Entities.BuyerAggregate
             Guard.Against.NullOrEmpty(identity, nameof(identity));
             IdentityGuid = identity;
         }
+
+        public string Street1 { get; private set; }
+        public string Street2 { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string ZipCode { get; private set; }
+
+        public void UpdateAddress(string street1, string street2, string city, string state, string zipCode)
+        {
+            // TODO: Validate address
+            Street1 = street1;
+            Street2 = street2;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+        }
     }
 }
